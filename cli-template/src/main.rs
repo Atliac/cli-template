438253e05b718bfac338fd545ce67e2fcad068da
Atliac <Atliac@outlook.com>
2026-06-23 16:22:48 +0800

    feat: add --name flag for personalized greeting

--- cli-template/src/main.rs
+++ cli-template/src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+/// A simple CLI template that greets you by name.
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    /// Your name
+    #[arg(short, long, default_value = "world")]
+    name: String,
+}
+
 fn main() {
-    println!("{}", env!("CARGO_PKG_VERSION"))
+    let args = Args::parse();
+    println!("Hello, {}!", args.name);
+    println!("cli-template v{}", env!("CARGO_PKG_VERSION"));
 }
